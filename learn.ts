716a19b2100fec24217942966cb6c5590e21f8d9
sung uk <sunguk0204@monkey-soft.com>
2022-03-15 15:50:51 +0900

    Last Commit

--- learn.ts
+++ learn.ts
@@ -3,7 +3,7 @@ export default function () {} // isolatedModules
 let message: string = 'Hello World';
 
 function sum(a: number, b: number) {
-    return a + b;
+  return a + b;
 }
 
 const result = sum(1, 2);
---
+++ screens/RootStack.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack';
+import {RouteProp, useRoute} from '@react-navigation/native';
+import MainTab, {MainTabNavigationScreenParams} from './MainTab';
+
+type RootStackParamList = {
+  MainTab: MainTabNavigationScreenParams;
+  Detail: {
+    id: number;
+  };
+};
+
+export type RootStackNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+type DetailScreenRouteProp = RouteProp<RootStackParamList, 'Detail'>;
+function DetailScreen() {
+  const {params} = useRoute<DetailScreenRouteProp>();
+  return (
+    <View>
+      <Text>Detail {params.id}</Text>
+    </View>
+  );
+}
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={MainTab}
+        name="MainTab"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen component={DetailScreen} name="Detail" />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
